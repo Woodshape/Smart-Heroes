169afb2c4b47f50fb2ba7a27449d81acfbd0305e
Woodshape <rkarschny@gmail.com>
2021-11-21 01:01:01 +0100

    Removed Seeker Ai

--- Assets/Scripts/GOAP/GAgent.cs
+++ Assets/Scripts/GOAP/GAgent.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 using UnityEngine.AI;
 
 public class GAgent : MonoBehaviour {
-    public Vector3 target = Vector3.zero;
+    public Transform target;
 
     public GameObject brain;
 
     public List<GAction> actions = new List<GAction>();
+    
+    private Vector3 lastTargetPosition = Vector3.zero;
     
     public Path path;
     public float speed = 2;
@@ -18,6 +20,7 @@ public class GAgent : MonoBehaviour {
     public bool reachedEndOfPath;
     
     // private NavMeshAgent _agent;
+    private IAstarAI _ai;
     private Seeker _seeker;
     
     private int currentWaypoint = 0;
@@ -29,6 +32,7 @@ public class GAgent : MonoBehaviour {
         // _agent.updateUpAxis = false;
 
         _seeker = GetComponent<Seeker>();
+        _ai = GetComponent<IAstarAI>();
 
         Goal goal = new Goal("win", 1, false);
 
@@ -37,76 +41,93 @@ public class GAgent : MonoBehaviour {
             foreach (GAction action in brain.GetComponentsInChildren<GAction>()) {
                 actions.Add(action);
                 
-                Debug.Log(action, gameObject);
+                Debug.Log($"GOAP -> {action}", gameObject);
             }
         }
         
         //  A* AI
-        Vector3 position = transform.position;
-        _seeker.StartPath(position, new Vector3(position.x + Random.Range(-10, -5),  position.y + Random.Range(-10, 10), 0), OnPathComplete);
+        // Vector3 position = transform.position;
+        // _seeker.StartPath(position, new Vector3(position.x + Random.Range(-10, -5),  position.y + Random.Range(-10, 10), 0), OnPathComplete);
     }
 
     // Update is called once per frame
     void Update() {
-        if (path == null) {
-            // We have no path to follow yet, so don't do anything
+        if (_ai == null) {
             return;
         }
         
-        // Check in a loop if we are close enough to the current waypoint to switch to the next one.
-        // We do this in a loop because many waypoints might be close to each other and we may reach
-        // several of them in the same frame.
-        reachedEndOfPath = false;
-        // The distance to the next waypoint in the path
-        float distanceToWaypoint;
-        while (true) {
-            // If you want maximum performance you can check the squared distance instead to get rid of a
-            // square root calculation. But that is outside the scope of this tutorial.
-            distanceToWaypoint = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
-            if (distanceToWaypoint < nextWaypointDistance) {
-                // Check if there is another waypoint or if we have reached the end of the path
-                if (currentWaypoint + 1 < path.vectorPath.Count) {
-                    currentWaypoint++;
-                } else {
-                    // Set a status variable to indicate that the agent has reached the end of the path.
-                    // You can use this to trigger some special code if your game requires that.
-                    reachedEndOfPath = true;
-                    break;
-                }
-            } else {
-                break;
-            }
+        if (target != null) {
+            _ai.destination = target.position;
         }
 
-        // Slow down smoothly upon approaching the end of the path
-        // This value will smoothly go from 1 to 0 as the agent approaches the last waypoint in the path.
-        var speedFactor = reachedEndOfPath ? Mathf.Sqrt(distanceToWaypoint/nextWaypointDistance) : 1f;
+        if (Vector3.Distance(lastTargetPosition, target.position) > 1f) {
+            _ai.SearchPath();
 
-        // Direction to the next waypoint
-        // Normalize it so that it has a length of 1 world unit
-        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
-        // Multiply the direction by our desired speed to get a velocity
-        Vector3 velocity = dir * speed * speedFactor;
+            Debug.Log("A* -> Target moved, updating pathfinding.");
+            
+            lastTargetPosition = target.position;
+        }
         
-
-        // If you are writing a 2D game you should remove the CharacterController code above and instead move the transform directly by uncommenting the next line
-        transform.position += velocity * Time.deltaTime;
-
-        if (reachedEndOfPath) {
-            Vector3 position = transform.position;
-            _seeker.StartPath(position, new Vector3(position.x + Random.Range(-10, 10),  position.y + Random.Range(-10, 10), 0), OnPathComplete);
-        }
+        // if (path == null) {
+        //     // We have no path to follow yet, so don't do anything
+        //     return;
+        // }
+        //
+        // // Check in a loop if we are close enough to the current waypoint to switch to the next one.
+        // // We do this in a loop because many waypoints might be close to each other and we may reach
+        // // several of them in the same frame.
+        // reachedEndOfPath = false;
+        // // The distance to the next waypoint in the path
+        // float distanceToWaypoint;
+        // while (true) {
+        //     // If you want maximum performance you can check the squared distance instead to get rid of a
+        //     // square root calculation. But that is outside the scope of this tutorial.
+        //     distanceToWaypoint = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+        //     if (distanceToWaypoint < nextWaypointDistance) {
+        //         // Check if there is another waypoint or if we have reached the end of the path
+        //         if (currentWaypoint + 1 < path.vectorPath.Count) {
+        //             currentWaypoint++;
+        //         } else {
+        //             // Set a status variable to indicate that the agent has reached the end of the path.
+        //             // You can use this to trigger some special code if your game requires that.
+        //             reachedEndOfPath = true;
+        //             break;
+        //         }
+        //     } else {
+        //         break;
+        //     }
+        // }
+        //
+        // // Slow down smoothly upon approaching the end of the path
+        // // This value will smoothly go from 1 to 0 as the agent approaches the last waypoint in the path.
+        // var speedFactor = reachedEndOfPath ? Mathf.Sqrt(distanceToWaypoint/nextWaypointDistance) : 1f;
+        //
+        // // Direction to the next waypoint
+        // // Normalize it so that it has a length of 1 world unit
+        // Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        // // Multiply the direction by our desired speed to get a velocity
+        // Vector3 velocity = dir * speed * speedFactor;
+        //
+        //
+        // // If you are writing a 2D game you should remove the CharacterController code above and instead move the transform directly by uncommenting the next line
+        // transform.position += velocity * Time.deltaTime;
+        //
+        // if (reachedEndOfPath) {
+        //     Vector3 position = transform.position;
+        //     _seeker.StartPath(position, new Vector3(position.x + Random.Range(-10, 10),  position.y + Random.Range(-10, 10), 0), OnPathComplete);
+        // }
     }
     
-    public void OnPathComplete (Path p) {
-        Debug.Log("A* -> A path was calculated. Did it fail with an error? " + p.error, gameObject);
-
-        if (!p.error) {
-            path = p;
-            // Reset the waypoint counter so that we start to move towards the first point in the path
-            currentWaypoint = 0;
-        }
-    }
+    // public void OnPathComplete (Path p) {
+    //     if (!p.error) {
+    //         path = p;
+    //         // Reset the waypoint counter so that we start to move towards the first point in the path
+    //         currentWaypoint = 0;
+    //     }
+    //     else {
+    //         Debug.LogWarning("A* -> Path was calculated with an error: " + p.errorLog, gameObject);
+    //     }
+    // }
     
     public class Goal {
         public Dictionary<string, int> goals;
